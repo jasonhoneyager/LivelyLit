0b62d2f9b5c1e15cd45e123d406a64b6f07d70c9
Jason Honeyager <jasonhoneyager@gmail.com>
2016-08-31 12:26:42 -0500

    Updated project class with working calendar popup and extended text area for description

---
+++ ProjectManager/ProjectManager/Controllers/ProjectController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProjectManager.Models;
+
+namespace ProjectManager.Controllers
+{
+    public class ProjectController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Project
+        public ActionResult Index()
+        {
+            return View(db.ProjectModels.ToList());
+        }
+
+        // GET: Project/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ProjectModels projectModels = db.ProjectModels.Find(id);
+            if (projectModels == null)
+            {
+                return HttpNotFound();
+            }
+            return View(projectModels);
+        }
+
+        // GET: Project/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Project/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,projectName,projectCategory,projectDescription,projectRequestedDueDate,projectOfferedPaymentType,projectOfferedPaymentAmount,projectPaymentMethod")] ProjectModels projectModels)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ProjectModels.Add(projectModels);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(projectModels);
+        }
+
+        // GET: Project/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ProjectModels projectModels = db.ProjectModels.Find(id);
+            if (projectModels == null)
+            {
+                return HttpNotFound();
+            }
+            return View(projectModels);
+        }
+
+        // POST: Project/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,projectName,projectCategory,projectDescription,projectRequestedDueDate,projectOfferedPaymentType,projectOfferedPaymentAmount,projectPaymentMethod")] ProjectModels projectModels)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(projectModels).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(projectModels);
+        }
+
+        // GET: Project/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ProjectModels projectModels = db.ProjectModels.Find(id);
+            if (projectModels == null)
+            {
+                return HttpNotFound();
+            }
+            return View(projectModels);
+        }
+
+        // POST: Project/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            ProjectModels projectModels = db.ProjectModels.Find(id);
+            db.ProjectModels.Remove(projectModels);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ProjectManager/ProjectManager/Models/ProjectModels.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProjectManager.Models
+{
+    public class ProjectModels
+    {
+        [Key]
+        public int ID { get; set; }
+        public string projectName { get; set; }
+        public string projectCategory { get; set; }
+        [DataType(DataType.MultilineText)]
+        public string projectDescription { get; set; }
+        public DateTime projectRequestedDueDate { get; set; }
+        public string projectOfferedPaymentType { get; set; } //per word, per assignment
+        public string projectOfferedPaymentAmount { get; set; }
+        public string projectPaymentMethod { get; set; } // paypal, check, escrow (paypal adaptive), etc...
+
+    }
+}
