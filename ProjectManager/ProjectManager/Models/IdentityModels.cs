13fe70b8072a331b5c2e13015a9de24fd6a97a7a
Jason Honeyager <jasonhoneyager@gmail.com>
2016-10-04 13:28:07 -0500

    updated frontend to look less ASP

--- ProjectManager/ProjectManager/Models/IdentityModels.cs
+++ ProjectManager/ProjectManager/Models/IdentityModels.cs
@@ -9,6 +9,15 @@ namespace ProjectManager.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        public string CompanyName { get; set; }
+        public string ContactName { get; set; }
+        public string Street1 { get; set; }
+        public string Street2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public string Phone { get; set; }
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
